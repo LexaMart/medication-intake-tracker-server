1eb6489ff1b9bba0490793577fab6082a5cc52fa
Aleksey Martinkevich <lexamart1708@gmail.com>
2024-09-01 23:05:31 +0300

    fix: change password regexp

--- src/dto/auth/auth-register-body.dto.ts
+++ src/dto/auth/auth-register-body.dto.ts
@@ -5,8 +5,7 @@ export type AuthRegisterBodyDto = {
 
 export const authRegisterBodyDtoValidator = (value: AuthRegisterBodyDto) => {
   const emailRegExp = /^[\w.-]+@[a-zA-Z\d.-]+.[a-zA-Z]{2,}$/;
-  const passwordRegExp =
-    /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
+  const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
 
   if (!emailRegExp.test(value.email)) {
     throw new Error("Email is incorrect");
